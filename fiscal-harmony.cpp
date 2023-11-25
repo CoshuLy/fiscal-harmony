f29d64794e05435ed88fdb406484da35ac3984f8
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2023-11-25 19:22:39 +0800

    Complete login system

--- fiscal-harmony.cpp
+++ fiscal-harmony.cpp
@@ -16,13 +16,22 @@ int main(){
     string userRegisterEmail, userRegisterPassword, userEmail, userPassword;
 
     cout << "=============================================================" << endl;
-    cout << "\t\tWelcome to Fiscal Harmony" << endl;
+    cout << "\t\t\t\tWelcome to Fiscal Harmony" << endl;
     cout << "=============================================================" << endl;
     cout << "Planning your future for a financial free life." << endl;
     cout << "[1]. Sign In [2]. Sign Up" << endl;
     cout << "|";
     cin >> userOption;
-
+    
+    if(userOption == 1){
+        cout << "_________________________________" << endl;
+        cout << "Username: ";
+        cin >> userEmail;
+        cout << "Password: ";
+        cin >> userPassword;
+        cout << "_________________________________" << endl;
+    }
+    
     if(userOption == 2){
         cout << "_________________________________" << endl;
         cout << "You are now creating an account" << endl;
@@ -32,17 +41,26 @@ int main(){
         cin >> userRegisterPassword;
         cout << "You have succesfully created an account, would you like to sign in?" << endl;
         cout << "[1]. Yes [2]. No" << endl;
+        cout << "|";
         cin >> userSignIn;
         cout << "_________________________________" << endl;
     }
-
+    
     if(userSignIn == 1){
-        cout << "_________________________________" << endl;
         cout << "Username: ";
         cin >> userEmail;
         cout << "Password: ";
         cin >> userPassword;
         cout << "_________________________________" << endl;
+        if(userEmail != userRegisterEmail || userPassword != userRegisterPassword){
+            while(userEmail != userRegisterEmail || userPassword != userRegisterPassword){
+                cout << "You have entered the wrong username or password, please try again!" << endl;
+                cout << "Username: ";
+                cin >> userEmail;
+                cout << "Password: ";
+                cin >> userPassword;
+                cout << "_________________________________" << endl;
+            }
+        }
+    cout << "Welcome, " << userEmail << " I hope you have a great time!";
     }
-
-}
