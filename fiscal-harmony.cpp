925c7869fe7fdf84435ab907ef3a895311c931bc
Rhyu Miranda <rhyu.agapito.miranda@gmail.com>
2023-11-29 17:14:17 +0800

    Add looping to invalid inputs

--- fiscal-harmony.cpp
+++ fiscal-harmony.cpp
@@ -11,21 +11,18 @@ int main(){
     cout << "=============================================================" << endl;
     cout << "Planning your future for a financial free life." << endl;
     cout << "[1]. Sign In [2]. Sign Up" << endl;
-    cout << "|";
+    cout << ">|";
     cin >> userOption;
 
     while (userOption != 1 && userOption != 2) {
         cout << "_________________________________" << endl;
-        if (cin >> userOption) {
-            // Valid input, break out of the loop
-            break;
-        } else {
-            // Invalid input, clear the error state and discard the invalid input
+        if (cin.fail() || (userOption != 1 && userOption != 2)) {
+            // Invalid input, clear the error state and discard the invalid input 
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
-
-            cout << "Invalid input. Please enter a valid number." << endl;
-            cout << "|";
+            // They need to input a valid number which is only 1 and 2, rather than 3 to ∞ or 0 to -∞, also invalid alphabetical letters
+            cout << "Invalid input. Please enter a valid number (1 or 2)." << endl;
+            cout << ">|";
         }
         cin >> userOption;
     }
@@ -70,23 +67,40 @@ int main(){
         cin >> userSignIn;
         cout << "_________________________________" << endl;
         
+        while (userSignIn != 1 && userSignIn != 2) {
+        cout << "_________________________________" << endl;
+        if (cin.fail() || (userSignIn != 1 && userSignIn != 2)) {
+            // Invalid input, clear the error state and discard the invalid input 
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            // They need to input a valid number which is only 1 and 2, rather than 3 to ∞ or 0 to -∞, also invalid alphabetical letters
+            cout << "Invalid input. Please enter a valid number (1 or 2)." << endl;
+            cout << ">|";
+        }
+            cin >> userSignIn;
+        }
+        
         if (userSignIn == 1){
             cout << "Username: ";
             cin >> userEmail;
             cout << "Password: ";
             cin >> userPassword;
             cout << "_________________________________" << endl;
-        if(userEmail != userRegisterEmail || userPassword != userRegisterPassword){
-            while(userEmail != userRegisterEmail || userPassword != userRegisterPassword){
-                cout << "You have entered the wrong username or password, please try again!" << endl;
-                cout << "Username: ";
-                cin >> userEmail;
-                cout << "Password: ";
-                cin >> userPassword;
-                cout << "_________________________________";
-                break;
+            if(userEmail != userRegisterEmail || userPassword != userRegisterPassword){
+                while(userEmail != userRegisterEmail || userPassword != userRegisterPassword){
+                    cout << endl;
+                    cout << "You have entered the wrong username or password, please try again!" << endl;
+                    cout << "Username: ";
+                    cin >> userEmail;
+                    cout << "Password: ";
+                    cin >> userPassword;
+                    cout << "_________________________________";
                 }
             }
+        } else if (userSignIn == 2){
+            cout << "Have a great day ahead!";
+        } else {
+            cout << "Invalid input";
         }
     }
     cout << "Welcome, " << userEmail << " I hope you have a great time!" << endl;
